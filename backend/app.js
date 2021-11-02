2438902af026a86638587cd0de480d0042e54844
Ana Biasetti <anabiasetti@protonmail.com>
2021-11-03 00:57:47 +0100

    Mise en place d'express

---
+++ backend/app.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+  next();
+});
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.post("/api/stuff", (req, res, next) => {
+  console.log(req.body);
+  res.status(201).json({
+    message: "Objet créé !",
+  });
+});
+
+app.use("/api/stuff", (req, res, next) => {
+  const stuff = [
+    {
+      _id: "oeihfzeoi",
+      title: "Mon premier objet",
+      description: "Les infos de mon premier objet",
+      imageUrl:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/2016_Minolta_Dynax_404si.JPG/640px-2016_Minolta_Dynax_404si.JPG",
+      price: 4900,
+      userId: "qsomihvqios",
+    },
+    {
+      _id: "oeihfzeomoihi",
+      title: "Mon deuxième objet",
+      description: "Les infos de mon deuxième objet",
+      imageUrl:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/2016_Minolta_Dynax_404si.JPG/640px-2016_Minolta_Dynax_404si.JPG",
+      price: 2900,
+      userId: "qsomihvqios",
+    },
+  ];
+  res.status(200).json(stuff);
+});
+
+module.exports = app;
